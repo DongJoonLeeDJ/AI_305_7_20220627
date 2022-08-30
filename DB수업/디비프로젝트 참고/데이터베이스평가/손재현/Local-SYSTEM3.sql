65a392e133a914e6b75ac2dd7a43b9c14f962c62
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-08-30 16:09:06 +0900

    db플젝 참고

--- DB수업/디비프로젝트 참고/데이터베이스평가/손재현/Local-SYSTEM3.sql
+++
@@ -1,8 +0,0 @@
-create user fashionsql identified by"1234"
-default tablespace "USERS"
-temporary tablespace "TEMP";
-
-
-grant "CONNECT" to fashionsql;
-grant "RESOURCE"to fashionsql;
-grant dba to fashionsql;
--- DB수업/디비프로젝트 참고/데이터베이스평가/양명지/local-system.sql
+++
@@ -1,7 +0,0 @@
--- 유저 생성
-create user shop2 identified by 1234
-    default tablespace users
-    temporary tablespace TEMP;
-
--- 권한 부여
-grant connect,resource,dba to shop2;
