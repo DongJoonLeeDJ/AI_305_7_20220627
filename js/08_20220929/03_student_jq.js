13cfbdc62af1b60f5fca57c66f8e21072f3b1dde
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-09-29 14:40:08 +0900

    Update 03_student_jq.js

--- js/08_20220929/03_student_jq.js
+++ js/08_20220929/03_student_jq.js
@@ -41,5 +41,48 @@ $(
             information.append(newStuentInfo)
         })
 
+        printOrderBy.on('click', function() {
+            
+            //중복체크 로직을 적기
+            const idx = students.findIndex(
+                function(item) {
+                    return item.beonho === Number(beonho.val())
+                }
+            )
+            if(idx!=-1) //이미 해당 번호가 있는 경우
+            {
+                alert('중복!')
+                return
+            }
+            resetInfo() //화면 리셋
+            //새로운 값 추가
+            students.push(new Student(name.val(), gender.val(),
+            Number(beonho.val())))
+            //정렬(객체 정렬할 때도 sort를 씀)
+            students.sort(function(a,b) {
+                return a.beonho - b.beonho
+            })
+
+            //정렬된 것을 print
+
+            //val = value = input태그의 value
+            const student = new Student(name.val(),
+            gender.val(),Number(beonho.val()))
+            
+            let newStuentInfo = $('<h1></h1>')
+            newStuentInfo.text(student.toString())
+            if(student.gender=='남')
+            {
+                newStuentInfo
+                .css('background-color','springgreen')
+                .css('color', 'blue')
+            } else {
+                newStuentInfo
+                .css('background-color','black')
+                .css('color', 'pink')
+            }
+            information.append(newStuentInfo)
+        })
+
     }
 )
