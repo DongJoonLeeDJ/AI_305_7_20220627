45c284d2df1484389cea13c2455b179fbfbbe79f
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-10-06 12:04:21 +0900

    Update Magician.java

--- djJava/MyHelloJava03/src/javastudy07/Magician.java
+++ djJava/MyHelloJava03/src/javastudy07/Magician.java
@@ -5,6 +5,9 @@ import javastudy03.GameCharacter;
 public class Magician extends GameCharacter {
 	private int mp;
 
+	public Magician() {
+		
+	}
 	public Magician(String id, String _Class, int mp) {
 		super(id, _Class);
 		this.mp = mp;
