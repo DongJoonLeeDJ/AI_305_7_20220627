bb5d41ce1aee190902cc735b8227337fb49e30bd
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-10-07 12:38:33 +0900

    Update HelloJava01_Sikdang.java

--- djJava/MyHelloJava05/src/javastudy02/HelloJava01_Sikdang.java
+++ djJava/MyHelloJava05/src/javastudy02/HelloJava01_Sikdang.java
@@ -19,9 +19,32 @@ public class HelloJava01_Sikdang {
 		foods.add(f);
 		foods.add(r);
 		foods.add(b);
+		//foods에 있는 음식들은 모두 eat을 호출할 수 있다.
+		//또한! eat이 각각 다 다르게 구현되어 있다.
 		for (Food food : foods) {
 			food.eat();
 		}
+		
+		//익명클래스
+		//Ramen 등과 다르게 이름이 아예 없는 클래스
+		//temp에서만 쓰이는 클래스임...
+		Food temp = new Food() {
+			
+			@Override
+			public void eat() {
+				System.out.println("기도하고 먹는다.");
+			}
+		};
+		temp.setName("교회식당 밥");
+		foods.add(temp);
+		
+		foods.add(new Food() {
+			
+			@Override
+			public void eat() {
+				System.out.println("묵상하고 먹는다.");
+			}
+		});
 	}
 
 }
