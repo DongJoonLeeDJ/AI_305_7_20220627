783afe7e2e138f1b3ace68697cadd1735c0d8a57
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-10-17 11:32:54 +0900

    fail페이지까지 추가함

---
+++ djJsp/HelloJSP03/WebContent/fail.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<style type="text/css">
+h1{color:violet; background:pink}</style>
+</head>
+<body>
+<%! String birth; %>
+<%
+birth = request.getParameter("birth");
+System.out.println(birth+"값은 이상함");
+%>
+<h1>당신은 잘못된 값을 입력했어요. <%=birth %>는 잘못된 값입니다.</h1>
+<a href="requestex.html">처음으로</a>
+</body>
+</html>
+
---
+++ djJsp/HelloJSP03/src/com/myjava/mytest/RequestResponse.java
@@ -0,0 +1,85 @@
+package com.myjava.mytest;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestResponse
+ */
+@WebServlet("/RequestResponse")
+public class RequestResponse extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RequestResponse() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		doAction(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doAction(request, response);
+	}
+	
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String str = request.getParameter("birth");
+		try {
+			int year = Integer.parseInt(str);
+			Date now = new Date(); //지금 시간 가져옴
+			
+			SimpleDateFormat formatter = 
+					new SimpleDateFormat("yyyy");
+			//올해를 스트링으로 바꾼 것 2022 라는 문자열이 나온다.
+			String NowYear = formatter.format(now);
+			int age = Integer.parseInt(NowYear)-year+1;
+			
+			if(age>=20)
+				response.sendRedirect("pass.jsp?age="+age);
+			else
+				response.sendRedirect("ng.jsp?age="+age);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(str);
+//			response.setContentType("text/html;charset=UTF-8");
+//			request.setCharacterEncoding("UTF-8");//post방식일 때 한글깨짐 방지
+			
+			response.sendRedirect("fail.jsp?birth="+str);
+		}
+		
+	
+	}
+
+}
+
+
+
+
+
+
+
+
+
