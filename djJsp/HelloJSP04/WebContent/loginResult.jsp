13fb7b566c2702d1847f3d0730e0fa13158a81c7
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-10-18 12:30:31 +0900

    로그인

---
+++ djJsp/HelloJSP04/WebContent/loginResult.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%! String Name, Phone, Gender, Hobby; %>
+<%
+	Name = (String)session.getAttribute("name");
+	Phone = (String)session.getAttribute("phone");
+	Gender = (String)session.getAttribute("gender");
+	Hobby = (String)session.getAttribute("hobby");
+%>
+<%=Name%>
+<%=Phone %>
+<h1><%=Gender %></h1>
+<h2><%=Hobby %></h2>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- djJsp/HelloJSP04/src/com/myjsp/mytest/LogInOk.java
+++ djJsp/HelloJSP04/src/com/myjsp/mytest/LogInOk.java
@@ -1,11 +1,17 @@
 package com.myjsp.mytest;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginOk
@@ -52,6 +58,58 @@ public class LogInOk extends HttpServlet {
 				strID,strPW);
 		
 		System.out.println(strQuery);
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); //jar에 있는 db 연결 소스를 참조하는 것
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Connection conn = null; //DB 연결에 관련된 객체들
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			//db연결에 필요한 정보들
+			//db위치, 스키마이름, 인코딩 등
+			String jdbcDriver = "jdbc:mysql://localhost:3306/jspdb?" + "useUnicode=true&characterEncoding=utf8&"
+					+ "serverTimezone=UTC";
+			String dbUser = "root";
+			String dbPass = "1234";
+			
+			//연결을 만듦
+			conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+			stmt = conn.createStatement(); // 쿼리문 실행 관련 객체 생성
+			
+			rs = stmt.executeQuery(strQuery);
+			
+			rs.next(); //처음 위치로 이동
+			
+			//테이블에서 값을 읽어옴
+			String strName = rs.getString("name");//getNString 아님
+			String strPhone = rs.getString("hp");
+			String strGender = rs.getString("gender");
+			String strHobby = rs.getString("hobby");
+			
+			if(strName != null) //결과가 있다면 
+			{
+				HttpSession httpsession = request.getSession();
+				httpsession.setAttribute("name", strName);
+				httpsession.setAttribute("phone", strPhone);
+				httpsession.setAttribute("gender", strGender);
+				httpsession.setAttribute("hobby", strHobby);
+				response.sendRedirect("loginResult.jsp");
+			}
+				
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally { //db 호출하고 쿼리문 날렸으면 무조건 닫아줘야 함
+			try {   //그래서 다른 사용자들도 쓰고 다시 쓸 수 있다.
+				stmt.close();
+				conn.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 	
 	}
 }
