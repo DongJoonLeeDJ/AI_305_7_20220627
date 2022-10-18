166d36f63a6189708df097bce2d892833c2381e7
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-10-18 10:00:04 +0900

    Create JoinOk.java

---
+++ djJsp/HelloJSP04/src/com/myjsp/mytest/JoinOk.java
@@ -0,0 +1,86 @@
+package com.myjsp.mytest;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class JoinOk
+ */
+@WebServlet({ "/JoinOk", "/joinok", "/JOINOK", "/JoinOK" })
+public class JoinOk extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public JoinOk() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doAction(request, response);	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doAction(request, response);
+	}
+	
+	protected void doAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		//회원가입 화면에서의 데이터들 불러옴
+		String strName = request.getParameter("name");
+		String strId = request.getParameter("id");
+		String strPw = request.getParameter("pw");
+		String strPhone = request.getParameter("phone") + "-" +
+				request.getParameter("phone2") + "-" +
+				request.getParameter("phone3");
+		String strGender = request.getParameter("gender");
+		
+		String[] hobbys = request.getParameterValues("hobby");
+		String strHobby = Arrays.deepToString(hobbys);
+		
+		System.out.println("Insert에 삽입될 값들");
+		System.out.println(strName);
+		System.out.println(strId);
+		System.out.println(strPw);
+		System.out.println(strPhone);
+		System.out.println(strGender);
+		System.out.println(strHobby);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
