39bc8eeff539f0be8b2553e7cb0986838f76e297
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-10-19 11:34:29 +0900

    dao dto

---
+++ djJsp/HelloJSP05/sql01.sql
@@ -0,0 +1,6 @@
+create table memberDTO (
+name varchar(100) not null,
+id varchar(100) primary key,
+pw varchar(100),
+gender varchar(100)
+);
---
+++ djJsp/HelloJSP05/src/com/javalec/daodto/MemberDAO.java
@@ -0,0 +1,19 @@
+package com.javalec.daodto;
+
+public class MemberDAO {
+	
+	private String url = "jdbc:mysql://localhost:3306/jspdb?" + "useUnicode=true&characterEncoding=utf8&"
+			+ "serverTimezone=UTC";
+	private String uid = "root";
+	private String upw = "1234";
+	
+	public MemberDAO() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver"); //jar에 있는 db 연결 소스를 참조하는 것
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+}
