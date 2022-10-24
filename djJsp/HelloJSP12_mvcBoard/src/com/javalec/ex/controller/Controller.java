057d10b004edffc3096a8df30750e228d937ec34
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-10-24 12:40:47 +0900

    list.jsp만 하면 기본 틀 완성

---
+++ djJsp/HelloJSP12_mvcBoard/src/com/javalec/ex/controller/Controller.java
@@ -0,0 +1,86 @@
+package com.javalec.ex.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.javalec.ex.command.BCommand;
+import com.javalec.ex.command.BListCommand;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("*.do")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		actionDo(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		actionDo(request, response);
+	}
+	
+	protected void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("utf-8");
+		String viewPage = null; //어떤 페이지로 갈지
+		BCommand command = null; //인터페이스 추가
+								//다형성 이용해서 다양한 클래스 활용
+		String uri = request.getRequestURI();
+		//프로젝트명
+		String conPath = request.getContextPath(); 
+		//슬래시 뒤에 오는 것들. insert.do 등
+		//substring : 자르는 것. 즉 conPath만큼 자름
+		String com = uri.substring(conPath.length());
+		
+		if(com.equals("/list.do")) {
+			command = new BListCommand();
+			command.execute(request, response);
+			viewPage = "list.jsp";
+		}
+		//viewPage로 이동함
+		RequestDispatcher dispatcher =
+				request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- djJsp/HelloJSP12_mvcBoard/src/com/javalec/ex/dao/BDao.java
+++ djJsp/HelloJSP12_mvcBoard/src/com/javalec/ex/dao/BDao.java
@@ -3,6 +3,7 @@ package com.javalec.ex.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 import javax.naming.Context;
@@ -36,6 +37,24 @@ public class BDao {
 					+ "bGroup, bStep, bIndent from "
 					+ "mvc_board order by bGroup desc,"
 					+ "bStep asc";
+			System.out.println(sql);
+			conn=ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				int bId = rs.getInt("bId");
+				String bName = rs.getString("bName");
+				String bTitle = rs.getString("bTitle");
+				String bContent = rs.getString("bContent");
+				Timestamp bDate = rs.getTimestamp("bDate");
+				int bHit = rs.getInt("bHit");
+				int bGroup = rs.getInt("bGroup");
+				int bStep = rs.getInt("bStep");
+				int bIndent = rs.getInt("bIndent");
+				
+				BDto dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
+				dtos.add(dto);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
