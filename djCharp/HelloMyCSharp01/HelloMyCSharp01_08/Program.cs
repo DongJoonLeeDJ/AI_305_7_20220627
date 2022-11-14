ee4bb6c96297a2f23ea15eb0a948c9fcdeb93905
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-11-14 15:55:52 +0900

    다풀었음

--- djCharp/HelloMyCSharp01/HelloMyCSharp01_08/Program.cs
+++ djCharp/HelloMyCSharp01/HelloMyCSharp01_08/Program.cs
@@ -10,6 +10,69 @@ namespace HelloMyCSharp01_08
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1번");
+            //1. 1부터 10까지 순차적으로 출력
+            //    - for, while로 해보기
+            for (int i = 1; i <= 10; i++)
+                Console.Write(i + " ");
+            int count = 1;
+            Console.WriteLine("\nwhile문 버전 \n");
+            while (count <= 10)
+            {
+                Console.Write(count + " ");
+                count++;
+            }
+            //2.a부터 b까지 순차적으로 출력
+            //- for, while로 해보기
+            Console.WriteLine("\n2번");// 줄 띄어쓰기
+            Console.WriteLine("a?");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("b?");
+            int b = int.Parse(Console.ReadLine());
+
+            //두 숫자 값 바꾸기
+            if(a>b)
+            {
+                int temp = a;
+                a = b; 
+                b = temp;
+            }
+
+
+            for (int i = a; i <= b; i++)
+                Console.Write(i + " ");
+            count = a;
+            Console.WriteLine("\nwhile문 버전 \n");
+            while (count <= b)
+            {
+                Console.Write(count + " ");
+                count++;
+            }
+            Console.WriteLine("\n3번");
+            for(int i = 1; i<=100; i++)
+            {
+                if(i%2==0)
+                    Console.Write(i + " ");
+            }
+            Console.WriteLine("\nwhile문 버전 \n");
+            count = 1;
+            while (count <= 100)
+            {
+                if(count%2==0)
+                    Console.Write(count + " ");
+                count++;
+            }
+            Console.WriteLine("\na부터 b까지 출력하되 역순 for");
+            for(int i = b; i>=a; i--)
+                Console.Write(i + " ");
+
+            Console.WriteLine("\na부터 b까지 출력하되 역순 while");
+            count = b;
+            while (count >= a)
+            {
+                Console.Write(count + " ");
+                count--;
+            }
         }
     }
 }
