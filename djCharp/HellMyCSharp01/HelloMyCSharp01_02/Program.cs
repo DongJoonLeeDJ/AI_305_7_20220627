56c5be3ee5123ce9bbd5557ab4ae553beaad0da3
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-11-14 10:17:54 +0900

    solve

--- djCharp/HellMyCSharp01/HelloMyCSharp01_02/Program.cs
+++ djCharp/HellMyCSharp01/HelloMyCSharp01_02/Program.cs
@@ -11,6 +11,14 @@ namespace HelloMyCSharp01_02
         static void Main(string[] args)
         {
             Console.WriteLine("안녕 난 " + 34 +"살이야");
+
+            Console.WriteLine("안녕, 내 이름은 이동준이고, 남자이며, 30대야");
+
+            int age = 30;
+            string name = "이동준";
+            Console.WriteLine("안녕, 내 이름은 "+ name+ "이고, 남자이며, "+ age+"대야");
+            Console.WriteLine(string.Format("안녕, 내 이름은 {0}이고, 남자이며, {1}대야",name,age);
+            Console.WriteLine($"안녕, 내 이름은{name}이고, 남자이며, {age}대야");
         }
     }
 }
