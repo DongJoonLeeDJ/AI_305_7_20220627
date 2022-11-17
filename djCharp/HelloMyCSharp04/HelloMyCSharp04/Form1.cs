2d478aee3b432135e3d82ec643cd5b726ae63b6c
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-11-17 16:34:41 +0900

    반복문까지 해봄

---
+++ djCharp/HelloMyCSharp04/HelloMyCSharp04/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloMyCSharp04
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 5;
+            int b = 10;
+            int c = 0;
+            c = a + b;
+            c = c - a;
+
+            string hello = "안녕";
+            Console.WriteLine(hello.Contains("안"));//True
+
+            Console.WriteLine("안녕하세요");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (char i = '가'; i <= '힣'; i++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+            Label label = new Label();
+            label.AutoSize = true;
+            label.Location = new Point(0, 400);
+            //for (char i = '가'; i <= '힣'; i++)
+            //{
+            //    label.Text += i;
+            //    if (i % 150 == 0)
+            //        label.Text += "\n";
+            //}
+            for (char i = '가'; i <= '힣'; i++)
+            {
+                label.Text += i;
+                if (i % 100 == 0)
+                    label.Text += "\n";
+            }
+            Controls.Add(label);
+        }
+    }
+}
