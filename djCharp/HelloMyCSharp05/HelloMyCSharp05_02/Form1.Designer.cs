bb93b93f72c30d9a8114afecf90034414cd20f97
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-11-18 12:34:01 +0900

    list+winform

--- djCharp/HelloMyCSharp05/HelloMyCSharp05_02/Form1.Designer.cs
+++ djCharp/HelloMyCSharp05/HelloMyCSharp05_02/Form1.Designer.cs
@@ -31,7 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.ListText = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -47,36 +47,36 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(30, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "리스트";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(30, 136);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.Size = new System.Drawing.Size(29, 12);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "추가";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(30, 233);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.Size = new System.Drawing.Size(29, 12);
             this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.label3.Text = "제거";
             // 
-            // label4
+            // ListText
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(124, 47);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 12);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
+            this.ListText.AutoSize = true;
+            this.ListText.Location = new System.Drawing.Point(124, 47);
+            this.ListText.Name = "ListText";
+            this.ListText.Size = new System.Drawing.Size(38, 12);
+            this.ListText.TabIndex = 3;
+            this.ListText.Text = "label4";
             // 
             // button1
             // 
@@ -86,6 +86,7 @@
             this.button1.TabIndex = 4;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -95,6 +96,7 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -104,6 +106,7 @@
             this.button3.TabIndex = 6;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -113,6 +116,7 @@
             this.button4.TabIndex = 7;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -122,6 +126,7 @@
             this.button5.TabIndex = 11;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -131,6 +136,7 @@
             this.button6.TabIndex = 10;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -140,6 +146,7 @@
             this.button7.TabIndex = 9;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -149,6 +156,7 @@
             this.button8.TabIndex = 8;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form1
             // 
@@ -163,7 +171,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ListText);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -179,7 +187,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label ListText;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
--- djCharp/HelloMyCSharp05/HelloMyCSharp05_02/Form1.cs
+++ djCharp/HelloMyCSharp05/HelloMyCSharp05_02/Form1.cs
@@ -12,10 +12,113 @@ namespace HelloMyCSharp05_02
 {
     public partial class Form1 : Form
     {
+        //Form1 클래스의 멤버 변수
         public List<string> list = new List<string>();
         public Form1()
         {
             InitializeComponent();
+            //초기 화면 세팅
+            //label4라는 글자 대신 ""을 넣고
+            //버튼 8개에 랜덤한 값을 넣어줄 것
+            ListText.Text = ""; //프로그램시작하면
+            //디자인창에 있는 label4라는 글자 대신 ""을 넣는다.
+            Random rand = new Random();
+            button1.Text = rand.Next(100).ToString();
+            button2.Text = rand.Next(100) + "";
+            button3.Text = ""+rand.Next(100);
+            button4.Text = rand.Next(100).ToString();
+            button5.Text = button1.Text;
+            button6.Text = button2.Text;
+            button7.Text = button3.Text;
+            button8.Text = button4.Text;
+        }
+        //button1~button4까지는 ListText에 글자를 추가할 것
+        private void button1_Click(object sender, EventArgs e)
+        {
+            list.Add(button1.Text);
+            ListText.Text = ""; //label에 있는 텍스트를 리셋해줌
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //list.Add(button2.Text);
+            list.Add( (sender as Button).Text );
+            ListText.Text = ""; //label에 있는 텍스트를 리셋해줌
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //list.Add(button2.Text);
+            list.Add((sender as Button).Text);
+            ListText.Text = ""; //label에 있는 텍스트를 리셋해줌
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //list.Add(button2.Text);
+            list.Add((sender as Button).Text);
+            ListText.Text = ""; //label에 있는 텍스트를 리셋해줌
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        //글자를 지우는 거
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //list.Add(button2.Text);
+            list.Remove((sender as Button).Text);
+            ListText.Text = ""; //label에 있는 텍스트를 리셋해줌
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //list.Add(button2.Text);
+            list.Remove((sender as Button).Text);
+            ListText.Text = ""; //label에 있는 텍스트를 리셋해줌
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //list.Add(button2.Text);
+            list.Remove((sender as Button).Text);
+            ListText.Text = ""; //label에 있는 텍스트를 리셋해줌
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //list.Add(button2.Text);
+            list.Remove((sender as Button).Text);
+            ListText.Text = ""; //label에 있는 텍스트를 리셋해줌
+            foreach (var item in list)
+            {
+                ListText.Text += item + " ";
+            }
         }
     }
 }
