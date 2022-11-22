8e4362f4e1132091d1a9715cca841d9865548409
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-11-22 12:26:19 +0900

    문제

---
+++ djCharp/HelloMyCSharp07/HelloMyCSharp07_01/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloMyCSharp07_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MyParent.money = 10000;
+            MyChild c = new MyChild();
+            Console.WriteLine(MyParent.money);
+            
+        }
+    }
+}
---
+++ djCharp/HelloMyCSharp07/HelloMyCSharp07_01/MyChild.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMyCSharp07_01
+{
+    public class MyChild : MyParent
+    {
+        public MyChild()
+        {
+            money--;
+        }
+    }
+}
