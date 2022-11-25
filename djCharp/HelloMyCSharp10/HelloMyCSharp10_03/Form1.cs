9e337c444520412ef36670fee67e1b099984d99c
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-11-25 11:54:41 +0900

    Update Form1.cs

--- djCharp/HelloMyCSharp10/HelloMyCSharp10_03/Form1.cs
+++ djCharp/HelloMyCSharp10/HelloMyCSharp10_03/Form1.cs
@@ -25,6 +25,8 @@ namespace HelloMyCSharp10_03
             {1,2,3,4,5,6,7,8,9,10 };
 
             //LINQ Language-Integrated Query 
+
+            //list에 있는 item들을 제곱해서 output에 하나하나 집어넣는다.
             var output = from item in list select item * item;
             //output은 list에 있는 값들을 전부 제곱해서
             //저장하는 일종의 리스트
@@ -40,11 +42,21 @@ namespace HelloMyCSharp10_03
             List<int> list = new List<int>()
             {1,2,3,4,5,6,7,8,9,10 };
 
+            //from 뒤에 있는 item : list에 있는 값들
+            //where : 조건
+            //select : 실제로 출력될 값
             var output = from item in list 
                          where item % 2 == 0 select item;
 
+            //원본인 list엔 변화가 없고
+            //output에 list의 특정값들만 집어넣음
+            //혹은 list값들을 가공한 것을 output에 넣는 것
             foreach (var item in output)
                 label2.Text += item + " ";
+
+            //LINQ의 결과물은 배열이나 List로 변환도 가능함
+            int[] a = output.ToArray<int>();
+            List<int> b = output.ToList<int>();
 
         }
     }
