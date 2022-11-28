1871aa846b66d499d57283b45cf7ad387e06a82c
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-11-28 11:56:26 +0900

    api

---
+++ djCharp/useLocalMap/useLocalMap/KakaoAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace useLocalMap
+{
+    public class KakaoAPI
+    {
+        //클래스 메소드 만듦
+        //KakaoAPI.Search("대구");
+        public static List<Locale> Search(string text)
+        {
+            List<Locale> list = new List<Locale>();
+
+            string site = "https://dapi.kakao.com/v2/local/search/keyword.json";
+            string query = $"{site}?query={text}";
+            string restApiKey = "894d02d154f5deca7c46014d9247a0ad";
+            string Header = $"KakaoAK {restApiKey}";
+            //Using System.Net
+            WebRequest request = WebRequest.Create(query);
+            request.Headers.Add("Authorization", Header);
+
+            return list;
+
+        }
+    }
+}
---
+++ djCharp/useLocalMap/useLocalMap/Locale.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace useLocalMap
+{
+    //위치정보 (위치이름, 그 위치의 좌표들)
+    public class Locale
+    {
+        public string name { get; set; }
+        public double Lat { get; set; } //Y
+        public double Lng { get; set; } //X
+
+        public Locale(string name, double lat, double lng)
+        {
+            this.name = name;
+            Lat = lat;
+            Lng = lng;
+        }
+        public override string ToString()
+        {
+            return name; //String으로 변환시 이름만 리턴
+        }
+    }
+}
