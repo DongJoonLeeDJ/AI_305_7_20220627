7448e121de4429cf34ce174d61338fa2478f85fc
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-11-29 16:38:41 +0900

    save load 기능 완성됨

--- djCharp/HelloMyCSharp11/HelloMyLastCSharp/Form1.cs
+++ djCharp/HelloMyCSharp11/HelloMyLastCSharp/Form1.cs
@@ -17,6 +17,15 @@ namespace HelloMyLastCSharp
             InitializeComponent();
             toolStripStatusLabel1.Text
                 = DateTime.Now.ToString("yyyy년 MM월 dd일 HH시 mm분 ss초");
+
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+
+            label5.Text = DataManager.Books.Count.ToString();
+            label6.Text = DataManager.Users.Count.ToString();
+        
         }
 
         private void timer1_Tick(object sender, EventArgs e)
