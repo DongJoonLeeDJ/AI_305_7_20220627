34cf2645d228a142c100c0f5dcb63ac716e8e9be
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-12-07 16:27:17 +0900

    Update Form1.cs

--- djCharp/HelloMyCSharp11/HelloMyCSharp11/Form1.cs
+++ djCharp/HelloMyCSharp11/HelloMyCSharp11/Form1.cs
@@ -17,19 +17,26 @@ namespace HelloMyCSharp11
             InitializeComponent();
 
             chart1.Series[0].Name = "롤 실력";
-            chart1.Series[0].Points.AddXY(1, 2);
-            chart1.Series[0].Points.AddXY(2, 3);
-            chart1.Series[0].Points.AddXY(3, 4);
+            chart1.Series[0].Points.AddXY("a", 2);
+            chart1.Series[0].Points.AddXY("b", 3);
+            chart1.Series[0].Points.AddXY("c", 4);
 
             //두번째 차트
             chart2.Series[0].Points.AddXY(0.0, 10);
             chart2.Series[0].Points.Add(new double[] { 2.0 });
             chart2.Series[0].Name = "level";
             chart2.Series.Add("data");
+            //chart2.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedBar;
             chart2.Series[0].Points.Add(new double[] { 5.0 });
             chart2.Series[1].Points.Add(new double[] { 20.0 });
             chart2.Series[1].Points.Add(new double[] { 50.0 });
-
+            chart2.Series[1].Points.Add(new double[] { 15.0 });
+            chart2.Series[0].Points[0].AxisLabel = "이동준";
+            chart2.Series[0].Points[1].AxisLabel = "이용희";
+            chart2.Series[0].Points[2].AxisLabel = "오세룡";
+            chart2.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            chart2.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedColumn;
+            
             //세번째 차트
             chart3.ChartAreas[0].AxisX.LabelStyle.Interval = 1;
             for(int i = 0; i<20; i++)
